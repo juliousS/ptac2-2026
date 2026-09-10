87ea0a0187183ad34ae2848b4132d0a2c51fc151
juliousS <julio.simoes@estudante.ifms.edu.br>
2026-09-10 13:40:45 +0000

    App.css

--- 4B-css001/src/App.jsx
+++ 4B-css001/src/App.jsx
@@ -1,17 +1,20 @@
-import './App.css'
-import Artigo from './components/artigo'
+import "./App.css"
+import Artigo from "./components/Artigo"
 
-export default function App(){
-  return(
-    <>
-    <h1>Titulo do Site</h1>
-    <p>PROGRAMAÇÃO E TECNOLOGIAS PARA APLICAÇÕES CLIENTE 2</p>
-    <ul>
-      <li>Item 01</li>
-      <li>Item 02</li>
-      <li>Item 03</li>
-    </ul>
-    <Artigo titulo="teste titulo"/>
-    </>
-  )
+export default function App() {
+    return (
+        <>
+            <h1>Título do site</h1>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dolore dolorem modi architecto tenetur impedit eveniet, fugiat quaerat laborum eius sint, rem praesentium reiciendis quasi nihil tempora odio eligendi quidem?</p>
+
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+
+            <Artigo titulo="Teste titulo"/>
+        </>
+    )
 }
---
+++ 4B-css001/src/components/Artigo.jsx
@@ -0,0 +1,13 @@
+import './Artigo.css'
+
+export default function Artigo(props){
+        return(
+            <>
+                <h1 className='titulo'>{props.titulo}</h1>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit qui nam quidem, ipsam asperiores incidunt vero non nostrum quam voluptate? Debitis beatae ea nam non tenetur accusantium architecto voluptatibus omnis!</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cumque veniam sequi exercitationem necessitatibus architecto nesciunt neque quia laudantium, vero nihil possimus voluptatum maiores numquam esse, quas laborum? Exercitationem, magni.</p>
+            </>
+        )    
+}
